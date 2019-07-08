b9114d540b4850b77d31f6d5e679c1ea47c522d2
hayatosann <sasano.hayato@icloud.com>
2019-07-08 21:44:18 +0900

    set-controller&route

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,9 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
 
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
