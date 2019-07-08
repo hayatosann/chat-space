89b9be665366d353cd3ea61ad4f47964980612f3
hayatosann <sasano.hayato@icloud.com>
2019-07-08 16:34:50 +0900

    set-routing-group

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
+
