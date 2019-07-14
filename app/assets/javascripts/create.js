43dd371968cb59c4f2b51c1da2a85098108a8d80
hayatosann <sasano.hayato@icloud.com>
2019-07-14 11:19:38 +0900

    fix-detail

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,20 +1,27 @@
 $(function() {
-
-  function buildMessage(message){
-    var html = 
-        `<div class = "message__upper-info">
-            <p class="message__upper-info__talker">
-              ${message.name}
-            </p>
-            <p class="message__upper-info__date">
-              ${message.created_at}
-            </p>
-          </div>
-            <p class="message__text">
-              ${message.content}
-              message.image == null ? "" :<img class= "lower-message__image" src=${message.image} >` 
-        return html;
+    function buildMessage(message){
+     var img =message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
+     var html = 
+              `<div class = "message__upper-info">
+                <p class="message__upper-info__talker">
+                  ${message.name}
+                </p>
+                <p class="message__upper-info__date">
+                  ${message.created_at}
+                </p>
+               </div>
+               <div class = "lower-message">
+                <p class="lower-message__content">
+                  ${message.content}
+                </p>
+                ${img}
+              </div>`
+    return html;
    }
+
+   function ScrollToNewMessage(){
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'slow');
+  }
 
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
@@ -34,9 +41,8 @@ $(function() {
       $('.messages').append(html);
 
       ScrollToNewMessage();
-	  	$(".form__message").val('');
-      $(".form__submit").prop('disabled', false);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'slow');
+	  	$("form")[0].reset();
+	  	$(".form__submit").prop('disabled', false);
     })
 
     .fail(function() {
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,8 +1,7 @@
 json.id @message.id
 json.content @message.content
-json.image @message.image
 json.user_id @message.user_id
-json.created_at @message.created_at.strftime("%Y/%m/%d/(%a) %H:%M:%S")
+json.created_at @message.created_at.strftime("%Y/%m/%d/(%a) %T")
 json.name @message.user.name
 json.image @message.image.url
 
