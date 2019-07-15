4a54538fbc9e9d2591169d7073324952e7e8d6d5
hayatosann <sasano.hayato@icloud.com>
2019-07-15 16:02:46 +0900

    Incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
-
+  
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.json
+    end
+  end
+ 
   def edit
   end
 
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -5,3 +5,4 @@ json.created_at @message.created_at.strftime("%Y/%m/%d/(%a) %T")
 json.name @message.user.name
 json.image @message.image.url
 
+
