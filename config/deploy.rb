db471ae97e61611024b5fdef528b596f48cdde80
hayatosann <sasano.hayato@icloud.com>
2019-07-18 01:00:50 +0900

    fix for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock '3.11.0'
 
-set :application, "chat-space"
+set :application, 'chat-space'
 set :repo_url, "git@github.com:hayatosann/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
