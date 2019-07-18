f2f8cd985eb4f4499db07e9cb20edc403caca423
hayatosann <sasano.hayato@icloud.com>
2019-07-18 20:30:11 +0900

    try

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'uploads-chatspace'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/uploads-chatspace'
+  config.asset_host = 'https://uploads-chatspace.s3-ap-northeast-1.amazonaws.com'
 end
